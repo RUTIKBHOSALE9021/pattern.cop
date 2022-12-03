ca118365a7575ad22624db7841ebc364580fda16
RUTIKBHOSALE9021 <rutikbhosale7218@gmail.com>
2022-12-03 21:51:15 +0530

    first commit

---
+++ pattern.cpp
@@ -0,0 +1,229 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  //  1 2 3 4 5
+  // 1 2 3 4 5
+  // 1 2 3 4 5
+  // 1 2 3 4 5
+  // 1 2 3 4 5
+
+  // int n;
+  // cout<<"enter number of rows and coloumn:";
+  // cin>>n;
+
+  // int i =1;
+  //  while (i<=n)
+  //  {
+  //    int j =1;
+  //    while (j<=n)
+  //    {
+  //     cout<<j<<" ";
+  //     j+=1;
+  //    }
+  //    cout<<endl;
+  //    i+=1;
+  //  }
+
+  // 5 4 3 2 1
+  // 5 4 3 2 1
+  // 5 4 3 2 1
+  // 5 4 3 2 1
+  // 5 4 3 2 1
+
+  // int num;
+  // cout<<"enter a number :";
+  // cin>>num;
+
+  // int i =1;
+  // while (i<=num)
+  // {
+  //     int j =1;
+  //    while (j<=num)
+  //    {
+  //     cout<<num-j+1<<" ";
+  //     j+=1;
+  //    }
+  //    i+=1;
+  //    cout<<endl;
+
+  // }
+
+  // 1 2 3
+  // 4 5 6
+  // 7 8 9
+  // int i=1;
+  // int count =1;
+
+  //  while(i<=3){
+  //     int j=1;
+  //     while(j<=3){
+  //         cout<<count<<" ";
+  //          count+=1;
+  //          j+=1;
+
+  //     }
+  //     i+=1;
+  //     cout<<endl;
+  //  }
+
+  // *
+  //  *  *
+  //  *  *  *
+  //  *  *  *  *
+  //  *  *  *  *  *
+  // int n;
+  // cout<<"enter a number";
+  // cin>>n;
+  // int row=1;
+
+  // while (row<=n)
+  // {
+  //    int col=1;
+  //    while(col<=row){
+  //     cout<<" *"<< " ";
+  //     col+=1;
+  //    }
+  //    row+=1;
+  //    cout<<endl;
+  // }
+
+  // 1
+  // 2 1
+  // 3 2 1
+  // 4 3 2 1
+
+  //     int n;
+  //     cout<<"enter a number " ;
+  //     cin>>n;
+  //     int count = 1;
+  //     int rows=1;
+  //     while (rows<=n)
+  //     { int cols=1;
+  //         while (cols<=rows)
+  //         {
+  //           cout<<rows-cols+1 <<" ";
+  //           cols+=1;
+  //         }
+
+  //         rows+=1;
+  //         cout<<endl;
+
+  //     }
+  // A A A A
+  // B B B B
+  // C C C C
+  // D D D D
+
+  //     int n;
+  //     cout << "enter a number";
+  //     cin >> n;
+  //   int i=1;
+  //     while (i <= n)
+  //     {
+  //       int j=1;
+  //         while (j <= n)
+  //         {
+  //             char ch = 'A' + i -1;
+  //             cout <<ch <<" ";
+  //             j += 1;
+  //         }
+  //         i += 1;
+  //         cout<<endl;
+  //     }
+
+  // A B C
+  // A B C
+  // A B C
+
+  // int n;
+  // cout<<"enter a number";
+  // cin>>n;
+  // int i =1;
+  // while (i<=n)
+  // {
+  //   int j=1;
+  //   while(j<=n)
+  //   {
+  //     char ch = 'A' + j-1;
+  //     cout<<ch<<" ";
+  //     j+=1;
+  //   }
+  //   i+=1;
+  //   cout<<endl;
+  // }
+
+  // A
+  // B B
+  // C C C
+  // int n;
+  // cout<<"enter a number";
+  // cin>>n;
+  // char ch = 'A';
+  // int i =1;
+  // while (i<=n)
+  // {
+  //   int j=1;
+  //   while(j<=i)
+  //   {
+  //     // char ch = 'A' + j-1;
+  //     char ch = 'A' + i-1;
+  //     cout<<ch<<" ";
+  //     j+=1;
+  //   }
+  //   i+=1;
+  //   cout<<endl;
+  // }
+
+  // A
+  // B C
+  // D E F
+  // G H I J
+
+  // int n;
+  // cout<<"enter a number";
+  // cin>>n;
+  // // char ch = 'A';
+  // char count = 'A';
+  // int i =1;
+  // while (i<=n)
+  // {
+  //   int j=1;
+  //   while(j<=i)
+  //   {
+  //     cout<<count<<" ";
+  //     count+=1;
+  //     j+=1;
+  //   }
+  //   i+=1;
+  //   cout<<endl;
+  // }
+
+  // D
+  // C D
+  // B C D
+  // A B C D
+// * * * *
+// * * *
+// * *
+// *
+//   int n;
+//   cout<<"enter a number";
+//   cin>>n;
+//   int i=1;
+//    while (i<=n)
+//    {
+//     int j =1;
+//     while (j<=n-i+1)
+//     {
+//      cout<<"*"<<" ";
+//      j+=1;
+//     }
+// i+=1;
+// cout<<endl;
+//    }
+   return 0;
+   }
+   
