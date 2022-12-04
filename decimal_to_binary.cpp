2eda67fb484ec193fc29dda84a610168adb4f148
RUTIKBHOSALE9021 <rutikbhosale7218@gmail.com>
2022-12-04 14:35:33 +0530

    first commit

---
+++ decimal_to_binary.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// #include<math.h>
+
+
+// // converting a decimal number to binary number;
+
+
+// using namespace std;
+//  int main(){
+//     int num;
+//     cout<<"enter a number: ";
+//     cin>>num;
+
+//     int sum=0;
+//     int i=0;
+
+//     while (num!=0)
+//     {
+//        int bit = num&1;
+
+//        sum = (bit * pow(10,i))+sum;
+//        num= num>>1;
+//        i++;
+//     }
+//     cout<<sum;
+//  }
